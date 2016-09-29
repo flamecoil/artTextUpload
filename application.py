178d81f58c5932d8b402f23cc379217ff0541b4c
flamecoil <flamecoil@yahoo.com>
2016-09-29 05:38:09 -0700

    Building dictionary for username variable management.

--- application.py
+++ application.py
@@ -18,7 +18,8 @@ class Application(Frame):
 		self.characterCount = 2
 		self.artistCount = 1
 		
-		self.charLabelList = list()
+		self.charStringList = list()
+		self.refStringList = list()
 		
 		self.row = 0
 		self.col = 0
@@ -35,10 +36,18 @@ class Application(Frame):
 		self.txtDisplayWords.grid(row=self.row, column=self.col)	 
 		"""
 		#Loop for Number of Characters
+		self.index = 0
+		while self.index < self.characterCount:
+			self.charString = StringVar()
+			self.charStringList.append(self.charString)
+			
+			self.refString = StringVar()
+			self.refStringList.append(self.refString)
+			self.index+=1
 		
 		#Characters
 		self.row += 1
-		self.lblPrompt = Label(self, text="Characters", anchor=W, width=self.labelWidth)
+		self.lblPrompt = Label(self, text="Characters: " + str(self.characterCount), anchor=W, width=self.labelWidth)
 		self.lblPrompt.grid(row=self.row, column=self.col)
 		
 		self.col+=1
@@ -49,17 +58,35 @@ class Application(Frame):
 			self.lblPrompt = Label(self, text="Name:", anchor=W, width=self.labelWidth)
 			self.lblPrompt.grid(row=self.row, column=self.col)
 			#Entry for Name
-			
-			#LOOP for Number of Links
+			self.entName1 = Entry(self, state=NORMAL, textvariable=self.charStringList[self.index])
+			self.entName1.grid(row=self.row, column=self.col+1)
+			#Link to Reference
+			self.row+=1
+			self.lblPrompt = Label(self, text="Reference URL:", anchor=W, width=self.labelWidth)
+			self.lblPrompt.grid(row=self.row, column=self.col)
+			#Entry for Name
+			self.entName1 = Entry(self, state=NORMAL, textvariable=self.refStringList[self.index])
+			self.entName1.grid(row=self.row, column=self.col+1)
+			#LOOP for Number of Links - USERNAMES
+			self.row+=1
+			self.lblPrompt = Label(self, text="Usernames", anchor=W, width=self.labelWidth)
+			self.lblPrompt.grid(row=self.row, column=self.col)
 			#Adjust for 
-			#Link to Character
+			#Link to Character Owner
 			#Owner of Character
+			self.row+=1
+			self.lblPrompt = Label(self, text="http://www.furaffinity.net/user/", anchor=E, width=self.labelWidth)
+			self.lblPrompt.grid(row=self.row, column=self.col)			
+			
+			
+			#LATER VERSIONS
 				#LOOP for Num_Sites
 				#Dropdown listing Site Types
 				# Specific sites, or general URL
 			self.index+=1
 	    
 		#Artist
+		self.col = 0
 		self.row += 1
 		self.lblPrompt = Label(self, text="Artists", anchor=W,width=self.labelWidth)
 		self.lblPrompt.grid(row=self.row, column=self.col)
